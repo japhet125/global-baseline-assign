7829ddb29992ad069499262333b9f29985385075
japhet125 <124853679+japhet125@users.noreply.github.com>
2026-02-15 16:25:52 -0500

    Add Quarto .qmd files and updating the code R

--- globalbaseline.R
+++ globalbaseline.R
@@ -1,8 +1,11 @@
 library(readr)
 library(dplyr)
 
+
+#loading the data from my github
 url <- "https://raw.githubusercontent.com/japhet125/global-baseline-assign/refs/heads/main/u.data"
 
+#data 
 ratings <- read_delim(
   url,
   delim = "\t",
@@ -11,3 +14,66 @@ ratings <- read_delim(
 )
 ratings <- ratings[, c("userId", "movieId", "rating")]
 head(ratings)
+
+#calculating the global average from all rating
+
+gm <- mean(ratings$rating, na.rm = TRUE)
+gm
+
+#calculating the users bias
+users_bias <- ratings %>%
+  group_by(userId) %>%
+summarise(
+  user_avg = mean(rating),
+  u_b = user_avg - gm
+  
+)
+users_bias
+
+#calculate the movie bias, we will use avg movie rating - glabal avg rating
+
+movies_bias <- ratings %>%
+  group_by(movieId) %>%
+  summarise(
+    movie_avg = mean(rating),
+    m_b = movie_avg - gm
+  )
+movies_bias
+
+#merging bias back to data to predict the rating
+
+ratings_pred <- ratings %>%
+  left_join(users_bias, by = "userId") %>%
+  left_join(movies_bias, by = "movieId") %>%
+  
+  mutate(
+    predicted_rating = gm + u_b + m_b
+  )
+
+ratings_pred
+  
+# making movie recommendation to user
+
+all_movies <- unique(ratings$movieId)
+
+user_id <- 1
+
+rated_movies <- ratings %>%
+  filter(userId == user_id) %>%
+  pull(movieId)
+
+unrated_movies <- setdiff(all_movies, rated_movies)
+
+unrated_movies
+
+#predicting ratings for those movies
+
+recommendation <- movies_bias %>%
+  filter(movieId %in% unrated_movies) %>%
+  left_join(users_bias %>% filter(userId == user_id), by = character()) %>%
+  mutate(
+    predicted_rating = gm + u_b + m_b
+  ) %>%
+  arrange(desc(predicted_rating))
+
+head(recommendation, 5)
--- windowtimes.R
+++ windowtimes.R
@@ -2,8 +2,12 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 
+#install.packages("zoo")
+library(zoo)
+
 set.seed(123)
-dates <- seq.Date(from = as.Date("2022-01-01"), to = as.Date("2022-12-31"), by = "day")
+dates <- seq.Date(from = as.Date("2022-01-01"), 
+                  to = as.Date("2022-12-31"), by = "day")
 crypto <- c("Bitcoin", "Ethereum")
 data <- expand.grid(date = dates, crypto = crypto)
 
@@ -12,3 +16,19 @@ data$price <- round(runif(nrow(data), 1000, 50000), 2)
 
 # Preview dataset
 head(data)
+
+#calculating window functions
+
+data_analysis <- data %>%
+  arrange(crypto, date) %>%
+  group_by(crypto) %>%
+  
+  mutate(ytd_avg = cummean(price)) %>%
+  
+  mutate( ma_6day = zoo::rollmean(price, k = 6, fill = NA, align = "right")) %>%
+  ungroup()
+  
+
+head(data_analysis, 10)  
+
+
